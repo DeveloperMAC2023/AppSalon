61c3b25a35a47be3c7a015eef1a524f6d035feb9
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-05-06 19:39:45 -0400

    Terminada la creación de cuenta de usuarios

---
+++ classes/Email.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+Class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token) {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username ='3ae3ae3b1bf88f';
+        $mail->Password = '0ce1f5b5a668a1';
+
+        $mail->setFROM('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com','AppSalon.com');
+        $mail->Subject = 'Confirma tu cuenta';
+
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p>Si tu no solicitaste una cuenta, puedes ignorar este mensaje</p>";
+        $contenido .= "</html>";
+
+        $mail->Body = $contenido;
+
+        // Enviar mail
+        $mail->send();
+
+    }
+}
--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 
 use MVC\Router;
+use Classes\Email;
+use Model\Usuario;
 
 class LoginController {
  
@@ -25,8 +27,59 @@ class LoginController {
     }
 
     public static function crear(Router $router) {
-        $router->render('auth/crear-cuenta', [
+        $usuario = new Usuario;
+        // Alertas vacias
+        $alertas = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {            
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validarNuevaCuenta();
+            
+            // Revisar que alertas este vacio
+            if (empty($alertas)) {
+                $resultado = $usuario->existeUsuario();
+                if ($resultado->num_rows) {
+                    $alertas = Usuario::getAlertas();
+                } else {
+                    // Hashear el password
+                    $usuario->hashPassword();                    
+                    // Generar un token único
+                    $usuario->crearToken();                      
+                    // Enviar email
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);                    
+                    // Enviar confirmación
+                    $email->enviarConfirmacion();
+                    // Crear el usuario
+                    $resultado = $usuario->guardar();
 
+                }
+            }
+        }
+        $router->render('auth/crear-cuenta', [
+            'usuario' => $usuario,
+            'alertas' => $alertas
+        ]);
+    }
+
+    public static function mensaje(Router $router) {
+        $router->render('auth/mensaje');
+    }
+
+    public static function confirmar(Router $router) {
+        $alertas = [];
+        $token = s($_GET['token']);
+        $usuario = Usuario::where('token',$token);
+        if (empty($usuario)) {
+            Usuario::setAlerta('error', 'Token no válido');
+        } else {
+            $usuario->confirmado = "1";
+            $usuario->token = null;
+            $usuario->guardar();
+            Usuario::setAlerta('exito','Cuenta Comprobada Correctamente');            
+        }
+
+        $alertas = Usuario::getAlertas();
+        $router->render('auth/confirmar-cuenta', [
+            'alertas' => $alertas
         ]);
     }
 
